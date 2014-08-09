91b819a6a5ba3ba33362e0c7cfe6b643e91f2446
Grant Timmerman <granttimmerman@gmail.com>
2014-08-09 16:48:17 -0700

    Allowed multiple sizes

--- canvas.js
+++ canvas.js
@@ -1,34 +1,53 @@
-var numImages = 100;
-var save = false;
+// 
+// Constants
+// 
 var Rainbow = require('color-rainbow');
-
 // The default image size should be pretty big. We will resize the images down later.
 var defaultImageSize = 1024; //px
 
+// 
+// Variables
+// 
+var sizes = [58, 80, 120];
+var numImages = 100;
+var save = false;
+
 function canvasToImg() {
   var canvas = document.getElementById("canvas");
-  canvas.setAttribute('width', defaultImageSize + 'px');
-  canvas.setAttribute('height', defaultImageSize + 'px');
   var ctx = canvas.getContext("2d");
-
+  
   var colors = Rainbow.create(numImages);
   var imgData = {};
-  for (var i = 0; i < numImages; ++i) {
-    var color = colors[i].hexString();
-    ctx.fillStyle = color;
-    ctx.fillRect(0, 0, defaultImageSize, defaultImageSize);
-    ctx.font = '900px Helvetica';
-    ctx.textAlign = 'center';
-    ctx.textBaseline = 'middle';
-    ctx.fillStyle = 'white';  // a color name or by using rgb/rgba/hex values
-    ctx.fillText(i+1, defaultImageSize/2, defaultImageSize/2);
 
-    var url = canvas.toDataURL();
-    imgData[i] = url;
+  
+  for (var j = 0; j < sizes.length; ++j) {
+    var size = sizes[j];
+    var scale = size/defaultImageSize;
 
-    var newImg = document.createElement("img");
-    newImg.src = url;
-    document.body.appendChild(newImg);
+    canvas.setAttribute('width', (scale * defaultImageSize) + 'px');
+    canvas.setAttribute('height', (scale * defaultImageSize) + 'px');
+
+    for (var i = 0; i < numImages; ++i) {
+      var color = colors[i].hexString();
+      var text = i + 1;
+      ctx.fillStyle = color;
+      ctx.fillRect(0, 0, defaultImageSize, defaultImageSize);
+      ctx.font = (scale * 900) + 'px Helvetica';
+      ctx.textAlign = 'center';
+      ctx.textBaseline = 'middle';
+      ctx.fillStyle = 'white';  // a color name or by using rgb/rgba/hex values
+      ctx.fillText(text, (scale * defaultImageSize)/2, (scale * defaultImageSize)/2);
+
+      var dataString = canvas.toDataURL();
+      var fileName = text + '_' + size + 'x' + size;
+      imgData[fileName] = dataString;
+
+      var newImg = document.createElement("img");
+      newImg.src = dataString;
+      document.body.appendChild(newImg);
+    }
+
+    document.body.appendChild(document.createElement('br'));
   }
   var data = JSON.stringify(imgData);
 
