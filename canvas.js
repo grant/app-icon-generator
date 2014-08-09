c779d91854d9062d8d06816206cf43fda089dcb9
Grant Timmerman <granttimmerman@gmail.com>
2014-08-09 14:02:06 -0700

    Add color rainbow

--- canvas.js
+++ canvas.js
@@ -1,15 +1,23 @@
 var imageSizes = [58, 80, 120];
 var numImages = 100;
 
+var Rainbow = require('color-rainbow');
+
 function canvasToImg() {
   var canvas = document.getElementById("canvas");
   var ctx = canvas.getContext("2d");
-  // //draw a red box
-  // ctx.fillStyle="#FF0000";
-  // ctx.fillRect(10,10,30,30);
 
-  // var url = canvas.toDataURL();
-  // console.log(url);
+  var colors = Rainbow.create(numImages);
+  for (var i = 0; i < numImages; ++i) {
+    var color = '#' + colors[i].hexString();
+    ctx.fillStyle = color;
+    console.log(color);
+    // ctx.fillRect(10,10,30,30);
+
+    // var url = canvas.toDataURL();
+    // console.log(url);
+  }
+    
 
   // var newImg = document.createElement("img"); //create
   // newImg.src = url;
