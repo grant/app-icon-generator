84aeed1f72f2f241cbf72eed639d76f1303ddf67
Grant Timmerman <granttimmerman@gmail.com>
2014-08-09 16:32:13 -0700

    Added numbers to image

--- canvas.js
+++ canvas.js
@@ -1,9 +1,9 @@
 var numImages = 100;
-
+var save = false;
 var Rainbow = require('color-rainbow');
 
 // The default image size should be pretty big. We will resize the images down later.
-var defaultImageSize = 124; //px
+var defaultImageSize = 1024; //px
 
 function canvasToImg() {
   var canvas = document.getElementById("canvas");
@@ -17,6 +17,11 @@ function canvasToImg() {
     var color = colors[i].hexString();
     ctx.fillStyle = color;
     ctx.fillRect(0, 0, defaultImageSize, defaultImageSize);
+    ctx.font = '900px Helvetica';
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'middle';
+    ctx.fillStyle = 'white';  // a color name or by using rgb/rgba/hex values
+    ctx.fillText(i+1, defaultImageSize/2, defaultImageSize/2);
 
     var url = canvas.toDataURL();
     imgData[i] = url;
@@ -28,7 +33,9 @@ function canvasToImg() {
   var data = JSON.stringify(imgData);
 
   var blob = new Blob([data], {type: "text/plain;charset=utf-8"});
-  saveAs(blob, "data.json");
+  if (save) {
+    saveAs(blob, "data.json");
+  }
 }
 
 canvasToImg();
