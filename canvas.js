7d257aced1f9d447b27ca50d4d31b8bbcd52f12c
Grant Timmerman <granttimmerman@gmail.com>
2014-08-09 16:20:26 -0700

    Add filesaver and add image preview

--- canvas.js
+++ canvas.js
@@ -3,12 +3,12 @@ var numImages = 100;
 var Rainbow = require('color-rainbow');
 
 // The default image size should be pretty big. We will resize the images down later.
-var defaultImageSize = 1024; //px
+var defaultImageSize = 124; //px
 
 function canvasToImg() {
   var canvas = document.getElementById("canvas");
-  canvas.style.width = defaultImageSize + 'px';
-  canvas.style.height = defaultImageSize + 'px';
+  canvas.setAttribute('width', defaultImageSize + 'px');
+  canvas.setAttribute('height', defaultImageSize + 'px');
   var ctx = canvas.getContext("2d");
 
   var colors = Rainbow.create(numImages);
@@ -20,12 +20,12 @@ function canvasToImg() {
 
     var url = canvas.toDataURL();
     imgData[i] = url;
-  }
-  document.getElementById('data').value = JSON.stringify(imgData);
 
-  // var newImg = document.createElement("img"); //create
-  // newImg.src = url;
-  // document.body.appendChild(newImg); //add to end of your document
+    var newImg = document.createElement("img");
+    newImg.src = url;
+    document.body.appendChild(newImg);
+  }
+  var data = JSON.stringify(imgData);
 }
 
 canvasToImg();
