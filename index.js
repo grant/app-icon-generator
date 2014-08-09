a931c91714a063f07589870338f2008b4815a2eb
Grant Timmerman <granttimmerman@gmail.com>
2014-08-09 13:28:00 -0700

    Separated html and js

---
+++ index.js
@@ -0,0 +1,16 @@
+function canvasToImg() {
+  var canvas = document.getElementById("canvas");
+  var ctx = canvas.getContext("2d");
+  // //draw a red box
+  // ctx.fillStyle="#FF0000";
+  // ctx.fillRect(10,10,30,30);
+
+  // var url = canvas.toDataURL();
+  // console.log(url);
+
+  // var newImg = document.createElement("img"); //create
+  // newImg.src = url;
+  // document.body.appendChild(newImg); //add to end of your document
+}
+
+canvasToImg();
